97bb32e1b7b8e7387e4950a46489250887e879d7
notAjack <notjack@tutanota.com>
2024-04-03 15:02:54 +0530

    changes

--- database.py
+++ database.py
@@ -4,16 +4,7 @@ school = sqlite3.connect("school.db")
 
 curs = school.cursor()
 
-curs.execute("""CREATE TABLE students (
-        admn_no integer,
-        name text,
-        parents_name text,
-        address text,
-        in_school text,
-        current_class integer
-        join_class_year integer
-        entrance_score integer
-    )""")
-
+curs.execute("SELECT * FROM students")
+print(curs.fetchall())
 school.commit()
 school.close()
