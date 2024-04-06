d9b8ce137edc89e2c6c0b44c1b83640a9d9f667b
notAjack <notjack@tutanota.com>
2024-04-06 10:54:32 +0530

    c

--- student_database/data_entering.py
+++ student_database/data_entering.py
@@ -2,17 +2,19 @@ import sqlite3
 
 studentorteacher = input("Enter Student Or Teacher: ")
 studentorteacher.lower
-
+student_columns = ("name", "parents_name", "address", "in_school", "current_class", "join_class_year", "entrance_score", "from_kg" )
+teacher_columns = ("name", "subject", "class_teacher_of")
 school = sqlite3.connect("school.db")
 curs = school.cursor()
 
 if studentorteacher == "teacher":
-    print(curs.execute("""SELECT page_count * page_size as size FROM pragma_page_count(), pragma_page_size()
-    """))
+    for i in teacher_columns:
+        i = input("Enter "+ i + ": ")
+        print(i)
+print(teacher_columns)
 
-elif studentorteacher == "student":    
-    print(curs.execute("""SELECT page_count * page_size as size FROM pragma_page_count(), pragma_page_size()
-    """))
+elif studentorteacher == "student":
+
 school.commit()
 school.close()
 
