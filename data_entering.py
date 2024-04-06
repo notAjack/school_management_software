7dea1242466c36da13f0bde162276ef06ebbf4fe
notAjack <notjack@tutanota.com>
2024-04-06 13:11:31 +0530

    c

--- data_entering.py
+++ data_entering.py
@@ -13,14 +13,13 @@ if studentorteacher == "teacher":
         teacher_columns[i] = (input("Enter " + teacher_columns[i] + ": "))
         i+=1        
     print(teacher_columns)
-    curs.execute("INSERT INTO teachers VALUES (eacher_columns)")
-
+    curs.execute("INSERT INTO teachers (teacher_name", "subject", "class_teacher_of) VALUES (?,?,?)", teacher_columns)
 elif studentorteacher == "student":
     for i in range(len(student_columns)):
         student_columns[i] = input("Enter " + student_columns[i] + ": ")
         i+=1
     print(student_columns)
-    curs.execute("INSERT INTO teachers VALUES (student_columns)")
+    curs.execute("INSERT INTO students ("student_name", "parents_name", "address", "in_school", "current_class", "join_class_year", "entrance_score", "from_kg") VALUES (?,?,?,?,?,?,?,?)", student_columns_columns)
 
 school.commit()
 school.close()
