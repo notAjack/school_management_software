49c957a57cd21a16c3836786c52738a5fa233a20
notAjack <notjack@tutanota.com>
2024-04-06 09:27:43 +0530

    c

--- student_database/database_student_data.py
+++ student_database/database_student_data.py
@@ -1,10 +1,10 @@
 import sqlite3
 
+studentorteacher = input("Enter Student Or Teacher")
+
 school = sqlite3.connect("school.db")
-
 curs = school.cursor()
-
-curs.execute("""
+curs.execute("""INSERT INTO school VALUES()
     """)
 print(curs.fetchall())
 school.commit()
