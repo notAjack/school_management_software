55489c5ca98191cbb2d6905aaf6c941a2578f05d
notAjack <notjack@tutanota.com>
2024-04-06 12:39:57 +0530

    c

--- student_database/data_entering.py
+++ student_database/data_entering.py
@@ -2,19 +2,14 @@ import sqlite3
 
 studentorteacher = input("Enter Student Or Teacher: ")
 studentorteacher.lower
-student_columns = ["name", "parents_name", "address", "in_school", "current_class", "join_class_year", "entrance_score", "from_kg" ]
-student_columns_values = []
-teacher_columns = ["name", "subject", "class_teacher_of"]
-teacher_columns_values = []
+student_columns = ["student_name", "parents_name", "address", "in_school", "current_class", "join_class_year", "entrance_score", "from_kg" ]
+teacher_columns = ["teacher_name", "subject", "class_teacher_of"]
 school = sqlite3.connect("school.db")
 curs = school.cursor()
 
 if studentorteacher == "teacher":
     for i in range(len(teacher_columns)):
-        j=0
-        teacher_columns_values.append(input("Enter "+ teacher_columns[i] + ": "))
-        print(teacher_columns_values[j])
-        j+=1
+        teacher_columns[i]=(input("Enter "+ teacher_columns[i] + ": "))
         i+=1        
 print(teacher_columns)
 
