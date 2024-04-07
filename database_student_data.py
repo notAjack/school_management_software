cb0d8f0b2e01d80c9a1eadc7283c46e42c59352a
notAjack <notjack@tutanota.com>
2024-04-07 15:28:21 +0530

    c

--- database_student_data.py
+++ database_student_data.py
@@ -1,5 +1,17 @@
-#classes
-#class_teacher
-#Fees
-#marks
-#attendance
+#classes class_teacher Fees marks attendance
+import sqlite3
+
+admn_no = input("Enter Admission Number of student: ")
+admn_no.lower
+
+school = sqlite3.connect("school.db")
+curs = school.cursor()
+curs.execute("""CREATE TABLE teachers (
+        teach_id integer PRIMARY KEY AUTOINCREMENT,
+        teacher_name text,
+        subject text,
+        class_teacher_of text
+    )""")
+
+school.commit()
+school.close()
