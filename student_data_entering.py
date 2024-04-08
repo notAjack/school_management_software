f957ca0a07c2440f744d575a4ce8b54058b76472
notAjack <notjack@tutanota.com>
2024-04-08 10:52:15 +0530

    c

--- student_data_entering.py
+++ student_data_entering.py
@@ -1,14 +1,23 @@
 import sqlite3
 
-admn_no = input("Enter Admission Number of student: ")
-admn_no.lower
+#admn_no = input("Enter Admission Number of student: ")
+#admn_no.lower
 #print(admn_no)
+oper = input("""
+Which Operation Would you like to do?
+Update class Teacher [Type teacher]
+Update Attendance    [Type attendance]
+Update Marks         [Type marks]
+Update fees          [Type fees]
 
-
+=>
+""")
+oper.lower
+print(oper)
 school = sqlite3.connect("school.db")
 curs = school.cursor()
 
-curs.execute("SELECT rowid FROM students")
+#curs.execute("SELECT rowid FROM students")
 
 school.commit()
 school.close()
