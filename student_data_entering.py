d5f616190255d902329585d82c3499e243556fd1
notAjack <notjack@tutanota.com>
2024-04-08 11:23:08 +0530

    c

--- student_data_entering.py
+++ student_data_entering.py
@@ -3,6 +3,16 @@ import sqlite3
 #admn_no = input("Enter Admission Number of student: ")
 #admn_no.lower
 #print(admn_no)
+clas = input("Enter Student's current class: ")
+clas = clas.lower()
+if clas == "lkg":
+    clas = 1
+elif clas == "ukg":
+    clas = 2
+else:
+    clas = int(clas)
+    clas = clas + 2
+#print(clas)
 
 oper = input("""
 Which Operation Would you like to do?
@@ -20,10 +30,14 @@ curs = school.cursor()
 
 #curs.execute("SELECT rowid FROM students")
 
-if oper == "teacher":
-elif oper == "attendance":
-elif oper == "marks":
-elif oper == "fees":
+#if oper == "teacher":
+
+
+#elif oper == "attendance":
+
+#elif oper == "marks":
+
+#elif oper == "fees":
 
 
 
