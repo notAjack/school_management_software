e5ca6fd6b03dfe6792f2410630a1ce9e46e8ba7f
notAjack <notjack@tutanota.com>
2024-04-08 09:27:27 +0530

    c

--- student_data_entering.py
+++ student_data_entering.py
@@ -1,11 +1,12 @@
 import sqlite3
+
 admn_no = input("Enter Admission Number of student: ")
 admn_no.lower
 print(admn_no)
+
+
 school = sqlite3.connect("school.db")
 curs = school.cursor()
-
-
 
 
 
